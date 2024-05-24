a2cb70993bd68eabc814ca4340cbcb711935e786
Mestre Santiago <souto.santiago@academico.ifpb.edu.br>
2024-05-24 20:28:52 -0300

    oi

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,22 @@
-import { Button } from "@/components/ui/button"
+import { LoginForm } from './components/form'
+import boxes from '../../assets/boxes.svg'
 
 export function Login() {
   return (
-    <>
-      <Button variant="destructive">Destructive</Button>
-    </>
+    <section className="w-screen h-screen d-flex justify-center items-center">
+      <div>
+        <div>
+          <div>
+            <h2>Bem vindo de Volta!</h2>
+            <p>Digite seu email e senha para fazer o login</p>
+          </div>
+          <LoginForm />
+        </div>
+        <div>
+          <img src={boxes} alt="Caixas" />
+        </div>
+      </div>
+      <p>@ 2024, Criado por Fast Order. Todos os direitos reservados.</p>
+    </section>
   )
 }
